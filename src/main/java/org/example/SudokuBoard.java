d61e694e15653e0be308297b8ec9fd2f3fa4f391
Piotr Żuchowski <piterowski2880@gmail.com>
2023-10-18 20:52:34 +0200

    Sudoku fillBoard algorithm

--- src/main/java/org/example/SudokuBoard.java
+++ src/main/java/org/example/SudokuBoard.java
@@ -1,2 +1,135 @@
-package org.example;public class SudokuBoard {
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class SudokuBoard {
+    private int[][] board = new int[9][9];
+    private int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    public SudokuBoard() {
+        shuffle();
+    }
+
+    public int[][] getBoard() {
+        int[][] copy = Arrays.copyOf(board, 9);
+        return copy;
+    }
+
+    public void fillBoard() {
+        for (int[] var : board) {
+            Arrays.fill(var, 0);
+        }
+
+        shuffle();
+        solveSudoku(9);
+    }
+
+    public boolean solveSudoku(int n) {
+        int row = 0;
+        int col = 0;
+        boolean isEmpty = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (!isEmpty) {
+                break;
+            }
+        }
+
+        // No empty space left
+        if (isEmpty) {
+            return true;
+        }
+
+        // Else for each-row backtrack
+        // for (int num = 1; num <= n; num++) {
+        for (int num : sequence) {
+            if (isSafe(row, col, num)) {
+                board[row][col] = num;
+                if (solveSudoku(n)) {
+                    // print(board, n);
+                    return true;
+                } else {
+                    // replace it
+                    board[row][col] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "SudokuBoard [board=" + Arrays.toString(board) + ", sequence=" + Arrays.toString(sequence) + "]";
+    }
+
+    public String getBoardString() {
+        String out = new String("");
+
+        for (int[] var : board) {
+            System.out.println(var);
+            for (int num : var) {
+                out += String.valueOf(num);
+                out += ", ";
+            }
+            out += "\n";
+        }
+        return out;
+    }
+
+    private void shuffle() {
+
+        List<Integer> sequenceList = Arrays.stream(sequence).boxed().collect(Collectors.toList());
+        Collections.shuffle(sequenceList, new Random());
+
+        for (int i = 0; i < 9; i++) {
+            sequence[i] = sequenceList.get(i);
+        }
+    }
+
+    private boolean isSafe(int row, int col, int num) {
+        // Row has the unique (row-clash)
+        for (int d = 0; d < board.length; d++) {
+
+            if (board[row][d] == num) {
+                return false;
+            }
+        }
+
+        // Column has the unique numbers (column-clash)
+        for (int r = 0; r < board.length; r++) {
+
+            if (board[r][col] == num) {
+                return false;
+            }
+        }
+
+        // Corresponding square has
+        // unique number (box-clash)
+        int sqrt = (int) Math.sqrt(board.length);
+        int boxRowStart = row - row % sqrt;
+        int boxColStart = col - col % sqrt;
+
+        for (int r = boxRowStart; r < boxRowStart + sqrt; r++) {
+            for (int d = boxColStart; d < boxColStart + sqrt; d++) {
+                if (board[r][d] == num) {
+                    return false;
+                }
+            }
+        }
+        // if there is no clash, it's safe
+        return true;
+    }
+
 }
--- src/test/java/org/example/SudokuBoardTest.java
+++ src/test/java/org/example/SudokuBoardTest.java
@@ -4,20 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class SudokuBoardTest {
-    @Test
-    void testSolveSudoku() {
-
-    }
-
-    @Test
-    void testFillboard() {
-
-    }
 
     @Test
     void ConstructorDoesNotThrow() {
         Assertions.assertDoesNotThrow(() -> {
-            SudokuBoard board = new SudokuBoard();
+            new SudokuBoard();
         });
     }
 
@@ -25,16 +16,37 @@ public class SudokuBoardTest {
     void solveSudokuDoesNotThrow() {
         Assertions.assertDoesNotThrow(() -> {
             SudokuBoard board = new SudokuBoard();
-            board.solveSudoku(9);
+            board.fillBoard();
         });
     }
 
     @Test
-    void fillBoardTest() {
+    void solveSudokuDoesntContainZero() {
+        SudokuBoard board = new SudokuBoard();
         Assertions.assertDoesNotThrow(() -> {
-            SudokuBoard board = new SudokuBoard();
-            board.solveSudoku(9);
+            board.fillBoard();
         });
+
+        for (int[] var : board.getBoard()) {
+            for (int num : var) {
+                System.out.print(String.format("%d ", num));
+            }
+            System.out.print('\n');
+        }
+    }
+
+    @Test
+    void genereateUniqueBoard() {
+        SudokuBoard board = new SudokuBoard();
+        board.fillBoard();
+        String out1 = board.getBoardString();
+        System.out.println(out1);
+
+        board.fillBoard();
+        String out2 = board.getBoardString();
+        System.out.println(out2);
+
+        Assertions.assertNotEquals(out1, out2);
     }
 
 }
